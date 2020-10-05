bf7e89957816da11477ae050d36e5205271cd177
nickjang <nickjang@alumni.upenn.edu>
2020-10-05 12:01:26 -0400

    Add start files.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import MainForm from 'MainForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    search: '',
+    type: '',
+    rating: ''
+  }
+
+  setSearch(search) {
+    this.setState({
+      search
+    });
+  }
+
+  setType(type) {
+    this.setState({
+      type
+    });
+  }
+
+  setRating(rating) {
+    this.setState({
+      rating
+    });
+  }
+
+  componentDidUpdate() {
+    // do a fetch or tell booklist to fetch
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+            <h1>Google Book Search</h1>
+        </header>
+        <main>
+          <MainForm setSearch={this.setSearch} setType={this.setType} setRating={this.setRating}/>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/MainForm.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import SearchBar from './SearchBar';
+import TypeFilter from './TypeFilter';
+import RatingFilter from './RatingFilter';
+
+class MainForm extends Component {
+  render() {
+    return (
+      <form id="main-form">
+            {/*Search Bar*/}
+            {/*Print type*/}
+            {/*rating */}
+        </form>
+    )
+  }
+}
+
+export default MainForm;
